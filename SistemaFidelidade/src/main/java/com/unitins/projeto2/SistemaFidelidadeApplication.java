e0ce1c123ab82b13b73299c3ff73ed3a3678a309
Gustavo Ferreira Parro <parrogustavo@gamil.com>
2020-09-17 21:38:32 -0300

    Iniciando projeto.

---
+++ SistemaFidelidade/src/main/java/com/unitins/projeto2/SistemaFidelidadeApplication.java
@@ -0,0 +1,13 @@
+package com.unitins.projeto2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaFidelidadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaFidelidadeApplication.class, args);
+	}
+
+}
